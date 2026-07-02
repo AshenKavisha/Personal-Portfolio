4ffac6d0ed4857f3a3edee4e35a6159e9459a3c3
ashen <kavishaashenkdh@gmail.com>
2026-07-02 18:03:24 +0530

    hero section updated

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -84,7 +84,7 @@ export const Hero = () => {
                 </span>
               </h1>
               <p className="text-lg text-muted-foreground max-w-lg animate-fade-in animation-delay-200">
-                Hi, I'm Pedro Machado — a software engineer specializing in
+                Hi, I'm Ashen Kavisha — a Data Sciencetist specializing in
                 React, Next.js, and TypeScript. I build scalable, performant web
                 applications that users love.
               </p>
